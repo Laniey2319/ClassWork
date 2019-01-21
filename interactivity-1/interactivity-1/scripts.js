38c6fe51daa66cfcf481e41591477aa1748ebbef
Laniey2319 <46456983+Laniey2319@users.noreply.github.com>
2019-01-21 15:00:53 -0500

    Add files via upload

---
+++ interactivity-1/interactivity-1/scripts.js
@@ -0,0 +1,43 @@
+// Interactivity demo 1
+
+console.log("Testing that this works")
+
+// Invert 
+var invertButton = document.querySelector("button") 
+var bodyClass = document.body.classList
+
+invertButton.addEventListener("click", changeColor) 
+
+function changeColor() { 
+	console.log("testing the color change")
+
+	if ( bodyClass.contains("invert") ) {
+		bodyClass.remove("invert")
+	} 
+
+	else {
+		bodyClass.add("invert")
+	}
+}
+
+
+// Greeting
+var hand=document.querySelector(".waving-hand")
+var greeting= document.querySelector(".greeting")
+
+hand.addEventListener("click", sayHello)
+
+function sayHello() {
+	greeting.textContent = "Oh, hello there friend."
+}
+
+
+// Fern
+var toggle = document.querySelector(".toggle")
+var content = document.querySelector(".content")
+
+toggle.addEventListener('click', showHide)
+
+function showHide() { 
+	content.id== "show" ? content.id = "" : content.id = "show"  
+} 
